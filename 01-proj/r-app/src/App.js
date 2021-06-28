68b07bc166274fca3b5a926e9689824dcc2eb908
Kirill Romanovskii <whitekvazar@gmail.com>
2021-06-28 16:46:48 +0300

    feat: start new react project add h1 and list

---
+++ 01-proj/r-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Header for random list</h1>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
