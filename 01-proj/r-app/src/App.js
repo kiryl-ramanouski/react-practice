559260adca6c0078e4852f8bd9212ed5a223f22b
Kirill Romanovskii <whitekvazar@gmail.com>
2021-06-28 22:14:56 +0300

    feat: add Footer component

--- 01-proj/r-app/src/App.js
+++ 01-proj/r-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Technologies from './Technologies';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
+      <Footer />
     </div>
   );
 }
---
+++ 01-proj/r-app/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <p>it-kamasutra.com footer</p>
+    </div>
+  )
+}
+
+export default Footer;
